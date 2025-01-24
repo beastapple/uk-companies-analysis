5731f15561a9e4303c4aa0b129c86500dd1d9a78
beastapple <123594847+beastapple@users.noreply.github.com>
2025-01-24 22:28:48 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown(""" # Companies Data Dashboard  
+There's nothing here yet! This application will allow for filtering and analysis of company information exported from Companies House.             
+""")
+
+st.divider()
