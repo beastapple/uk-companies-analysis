c16b1bd5b282f925ed7b49914b127957182dcd2a
beastapple <123594847+beastapple@users.noreply.github.com>
2025-02-09 21:16:05 +0000

    Update app.py
    Cleaned up the code a bit (more function focused)
    Added the ability to count the number of SIC codes ('nature_of_business' column)

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.markdown(""" # Companies Data Dashboard  
 This application will allow for filtering and analysis of company information exported from the [Companies House Advanced Search function](https://find-and-update.company-information.service.gov.uk/advanced-search).         
@@ -28,7 +30,7 @@ def deduplicate_data(df, dedupe_column):
     return deduped
 
 if upload_done:
-    st.write(f"Processing the following files: {raw_companies_data}") # Need to figure out how to get it to just say the name and not the full shebang of info.
+#    st.write(f"Processing the following files: {raw_companies_data}") # Need to figure out how to get it to just say the name and not the full shebang of info.
     all_files = pd.concat(pd.read_csv(csv_file) for csv_file in raw_companies_data)
 
 deduped = all_files.copy()
@@ -39,20 +41,42 @@ deduped = deduplicate_data(all_files, 'company_number') # Would be cool to add t
 # st.write(f"The dataset is {len(deduped)} rows long.")
 # st.write(deduped.columns.values)
 
-# Step 2. Validate the CSV upload is the right format/from Companies House and display the dashboard.
+# Step 2. Validate the CSV upload is the right format/from Companies House.
 
 # Creates a column list to validate the CSV upload against.
 companies_house_headers = pd.DataFrame(columns=['company_name', 'company_number', 'company_status', 'company_type', 'company_subtype', 'dissolution_date', 'incorporation_date', 'removed_date', 'registered_date', 'nature_of_business', 'registered_office_address'])
 
-if len(deduped) > 1:
+def validate_csv():
     unvalidated_csv = deduped
 #    st.write(unvalidated_csv.columns.values)
     if unvalidated_csv.columns.values.all() == companies_house_headers.columns.values.all():
         st.write("CSV validated.")
     else:
         st.write(f"Unexpected columns. Expected {companies_house_headers}")
-    
-    # Dashboard
+
+# Step 3. Wrangles the SIC codes (nature_of_business column) for analysis.
+
+def explode_sic(): # https://www.geeksforgeeks.org/how-to-split-explode-pandas-dataframe-string-entry-to-separate-rows/
+    # Explode the SICs column so that companies with multiple SICs are accounted properly.
+    sics_exploded = deduped.copy()
+    sics_exploded = sics_exploded['nature_of_business'].str.split().explode() # Returns as a series, not a dataframe
+    sics_counted = sics_exploded.groupby(sics_exploded).count() # https://stackoverflow.com/questions/33483670/how-to-group-a-series-by-values-in-pandas
+    sics_counted = sics_counted.sort_values(ascending=False)
+    st.write(sics_counted)
+    fig_sics = px.pie(
+        sics_exploded.head(30), 
+        values='nature_of_business', 
+        names='nature_of_business',
+        title='Top 30 SIC codes',
+        hole=0.3,
+        )
+    st.plotly_chart(fig_sics)
+
+
+
+# Step 3. Generates a dashboard.
+
+def dashboard():
     total_companies = len(deduped['company_number'])
     total_active = deduped['company_status'].value_counts().get('Active', 0)
     total_private = deduped['company_type'].value_counts().get('Private limited company', 0)
@@ -67,5 +91,11 @@ if len(deduped) > 1:
     with right_column:
         st.subheader("Total Private Limited Companies")
         st.subheader(f"{total_private:,}")
+    return st.write(deduped)
 
-    st.write(deduped)
+# Main 
+if len(deduped) > 1:
+    validate_csv()
+    dashboard()
+    explode_sic()
+
